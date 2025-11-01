69321c205971efa363ef080c42f8d2d3eb1967f0
Daniel Green <dg683@cam.ac.uk>
2025-11-01 21:28:44 +0000

    Main process warning system - email on uncaught exception

--- optimiser/optimiser.py
+++ optimiser/optimiser.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
+import traceback;
+
 import args;
 
 import db;
+
+import send_email;
+
+if __name__=="__main__":
+    try:
+        # Main program goes here - thus an uncaught exception will trigger an email warning
+
+        pass; # TODO 
+
+        # Testing warning system
+        # raise FutureWarning()
+    
+    except BaseException as e:
+        send_email.send_email(f"FAILURE - `optimiser.py` on {send_email.hostname()}", f"""\
+Optimiser Process failed on {send_email.hostname()}:
+
+{str(e)}
+
+Trace:
+{traceback.format_exc()}
+""");
+        raise e;
